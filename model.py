50985c354ad01f968768340b4fc0b9251d1c2185
Hitesh <giiteducomp@gmail.com>
2026-07-16 14:17:16 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ dataset['test_score'] = dataset['test_score'].fillna(dataset['test_score'].mean(
 
 X = dataset.iloc[:, :3]
 
-#Converting words to integer values
+#Converting words to integer valuesp
 def convert_to_int(word):
     word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
                 'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
